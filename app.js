9fffeade70de0c8db794bdec164ff248fc06e384
Gogs <gogs@fake.local>
2019-10-20 11:42:42 +0100

    switch to koa, add debugs

--- app.js
+++ app.js
@@ -1,60 +1,23 @@
-var port = process.env.PORT || 3000,
-    http = require('http'),
-    fs = require('fs');
+const Koa = require('koa');
+const app = new Koa();
+const util = require('util');
+const port = process.env.PORT || 3000
+const { spawnSync} = require('child_process');
 
-var app = http.createServer(function (req, res) {
-  if (req.url.indexOf('/img') != -1) {
-    var filePath = req.url.split('/img')[1];
-    fs.readFile(__dirname + '/public/img' + filePath, function (err, data) {
-      if (err) {
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.write('Error 404: Resource not found.');
-        console.log(err);
-      } else {
-        res.writeHead(200, {'Content-Type': 'image/svg+xml'});
-        res.write(data);
-      }  
-      res.end();
-    });
-  } else if (req.url.indexOf('/js') != -1) {
-    var filePath = req.url.split('/js')[1];
-    fs.readFile(__dirname + '/public/js' + filePath, function (err, data) {
-      if (err) {
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.write('Error 404: Resource not found.');
-        console.log(err);
-      } else {
-        res.writeHead(200, {'Content-Type': 'text/javascript'});
-        res.write(data);
-      }  
-      res.end();
-    });
-  } else if(req.url.indexOf('/css') != -1) {
-    var filePath = req.url.split('/css')[1];
-    fs.readFile(__dirname + '/public/css' + filePath, function (err, data) {
-      if (err) {
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.write('Error 404: Resource not found.');
-        console.log(err);
-      } else {
-        res.writeHead(200, {'Content-Type': 'text/css'});
-        res.write(data);
-      }
-      res.end();
-    });
-  } else {
-    fs.readFile(__dirname + '/public/index.html', function (err, data) {
-      if (err) {
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.write('Error 404: Resource not found.');
-        console.log(err);
-      } else {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-      }
-      res.end();
-    });
-  }
-}).listen(port, '0.0.0.0');
+app.use(async ctx => {
+    var body = ""
+    if (ctx.request.query.q != undefined) {
+        var q = ctx.request.query.q;
+	console.log("fetching " + q);
+    }
+    if (ctx.request.query.debug != undefined) {
+        body += util.inspect(ctx)
+	body += "\n\n"
+        const child = spawnSync('df', ['-h']);
+	body += child.stdout
+    }
+    ctx.body = body
+});
 
-module.exports = app;
+app.listen(port);
+console.log("Listening on " + port);
